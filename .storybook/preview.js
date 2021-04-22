59ff2df50283ffd3517c228380e6b3dbc94b26be
hila-abachi <igalk474@gmail.com>
2021-04-22 04:52:42 +0300

    fixes

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { addDecorator } from '@storybook/react';
-import '@coreui/coreui/dist/css/coreui.min.css';
+import '@coreui/coreui-pro/dist/css/coreui.min.css';
 import CRow from '../src/grid/CRow'
 import CContainer from '../src/grid/CContainer'
 import CCol from '../src/grid/CCol'
@@ -10,9 +10,9 @@ import CCard from '../src/card/CCard'
 addDecorator(story => (
   <>
     <CContainer fluid>
-        <CRow> 
+        <CRow>
             <CCol lg="12" xs="12">
-                <CCard lg="12" xs="12"> 
+                <CCard lg="12" xs="12">
                     <h1 className="ml-3">CoreUI</h1>
                 </CCard>
             </CCol>
